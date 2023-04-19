18232153c759b61c1384daf1678a6fd926888f55
Arafat Motolani <mhyzdamilola@gmail.com>
2023-04-19 14:29:09 -0700

    task 6

--- models/base_model.py
+++ models/base_model.py
@@ -2,10 +2,21 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, DATETIME
+from models import storage_type
+
+Base = declarative_base()
 
 
 class BaseModel:
-    """A base class for all hbnb models"""
+    """A base class for all hbnb models
+    These class will define common attributes for other classes or methods
+    """
+    id = Column(String(60), unique=True, nullable=False, primary_key=True)
+    created_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
+    updated_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
@@ -35,10 +46,15 @@ class BaseModel:
 
     def to_dict(self):
         """Convert instance into dict format"""
-        dictionary = {}
-        dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
-        return dictionary
+         my_dict = self.__dict__.copy()
+         my_dict['__class__'] = self.__class__.__name__
+        for k in my_dict:
+            if type(my_dict[k]) is datetime:
+                my_dict[k] = my_dict[k].isoformat()
+        if '_sa_instance_state' in dct.keys():
+            del(my_dict['_sa_instance_state'])
+        return my_dict
+    def delete(self):
+        """ delete object
+        """
+        models.storage.delete(self)
