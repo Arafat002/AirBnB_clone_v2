62a251899ddce1e9c235f3791d529dded2ba46d6
Mondabutu <mondabutu@gmail.com>
2023-04-19 23:30:19 +0100

    Console Improvement

--- console.py
+++ console.py
@@ -17,7 +17,7 @@ from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
-    """ Contains the functionality for the HBNB console"""
+    """ This Contains the functionality for the HBNB console"""
 
     # determines prompt for interactive/non-interactive modes
     prompt = '(hbnb) ' if sys.__stdin__.isatty() else ''
